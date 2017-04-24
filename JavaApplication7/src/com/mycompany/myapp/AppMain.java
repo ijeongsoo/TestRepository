f8265f904957067bad83e688a280a20b5981872b
ijeongsoo <quintessence6083@gmail.com>
2017-04-24 18:12:40 +0900

    4_24 Final

--- JavaApplication7/src/com/mycompany/myapp/AppMain.java
+++ JavaApplication7/src/com/mycompany/myapp/AppMain.java
@@ -1,10 +1,28 @@
 
 package com.mycompany.myapp;
 
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class AppMain {
+
+public class AppMain extends Application{
+
+	  @Override
+	  public void start(Stage primaryStage) throws Exception {
+			Parent parent = FXMLLoader.load(getClass().getResource("root.fxml"));
+			Scene scene = new Scene(parent);
+	  }
+	  
+	  
+	  
 	  public static void main(String[] args) {
 			System.out.println("내용1");
 			System.out.println("내용1");
+			AppMain.launch();
 	  }
+
 }
---
+++ WebApplication/web/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>WebApplication</title>
+  </head>
+  
+  <body>
+    WebApplication Home
+    <hr/>
+  </body>
+</html>
